6a50dcd5d86f66481a7cdfab26292a5304cd7808
HDHWANG-DESKTOP\hadon <hadong0624@naver.com>
2022-09-04 17:33:46 +0900

    대시보드 > API 호출로 변경

--- note/views/dashboard.py
+++ note/views/dashboard.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import TemplateView, View
 from note import models
 from note.util.formatHelper import *
+from note.util.tokenHelper import get_user_token
 
 import logging
+import requests
 logger = logging.getLogger(__name__)
-
 
 # 대시보드
 class DashboardView(TemplateView):
@@ -16,45 +18,40 @@ class DashboardView(TemplateView):
         return self.render_to_response(self.context)
 
 
-# 계좌번호 수 조회
-class BankAccountCntAPI(View):
+class CountAPIView(View):
+    base_url = getattr(settings, 'API_BASE_URL')
+    sub_path = ''
+
     def get(self, request):
         try:
-            return JsonResponse({'count': models.BankAccount.objects.filter().count()})
+            params = {'page_size': 1}
+            headers = {'Authorization': f'Token {get_user_token(request)}', 'Content-Type': 'application/json'}
+            response = requests.get(f'{self.base_url}/{self.sub_path}', params=params, headers=headers, verify=False)
+            if response.status_code == 200 and response.json():
+                count = response.json().get('count')
+
+            return JsonResponse({'count': count})
 
         except Exception as e:
-            logger.warning(f'[BankAccountCntAPI - get] {to_str(e)}')
+            logger.warning(f'[CountAPI - get] {to_str(e)}')
             return HttpResponse(status=400)
+
+
+# 계좌번호 수 조회
+class BankAccountCntAPI(CountAPIView):
+    sub_path = 'bank-account'
 
 
 # 시리얼 번호 수 조회
-class SerialCntAPI(View):
-    def get(self, request):
-        try:
-            return JsonResponse({'count': models.Serial.objects.filter().count()})
-
-        except Exception as e:
-            logger.warning(f'[SerialCntAPI - get] {to_str(e)}')
-            return HttpResponse(status=400)
+class SerialCntAPI(CountAPIView):
+    sub_path = 'serial'
 
 
 # 노트 수 조회
-class NoteCntAPI(View):
-    def get(self, request):
-        try:
-            return JsonResponse({'count': models.Note.objects.filter().count()})
-
-        except Exception as e:
-            logger.warning(f'[NoteCntAPI - get] {to_str(e)}')
-            return HttpResponse(status=400)
+class NoteCntAPI(CountAPIView):
+    sub_path = 'note'
 
 
 # 결혼식 방명록 조회
-class GuestBookCntAPI(View):
-    def get(self, request):
-        try:
-            return JsonResponse({'count': models.GuestBook.objects.filter().count()})
-
-        except Exception as e:
-            logger.warning(f'[GuestBookCntAPI - get] {to_str(e)}')
-            return HttpResponse(status=400)
+class GuestBookCntAPI(CountAPIView):
+    sub_path = 'guest-book'
