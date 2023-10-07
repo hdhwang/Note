eb3dfdc4c94b020091b5066504572a924bf63829
hdhwang <hadong0624@naver.com>
2023-10-07 23:28:24 +0900

    api 경로 변경

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,5 +3,5 @@ from .base import *
 DEBUG = True
 
 BASE_URL = "http://127.0.0.1:8080"
-API_BASE_URL = f"{BASE_URL}/note/v1"
+API_BASE_URL = f"{BASE_URL}/api/v1"
 JWT_BASE_URL = f"{BASE_URL}/token"
--- config/settings/production.py
+++ config/settings/production.py
@@ -3,5 +3,5 @@ from .base import *
 DEBUG = False
 
 BASE_URL = "https://hdhwang.duckdns.org:8080"
-API_BASE_URL = f"{BASE_URL}/note/v1"
+API_BASE_URL = f"{BASE_URL}/api/v1"
 JWT_BASE_URL = f"{BASE_URL}/token"
