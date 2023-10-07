614796e9500b35ebd0d58afd011815fe80cfe748
hdhwang <hadong0624@naver.com>
2023-10-07 22:04:33 +0900

    production 구동 시 BASE_URL 수정

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,7 @@
 from .base import *
 
 DEBUG = True
+
+BASE_URL = "http://127.0.0.1:8080"
+API_BASE_URL = f"{BASE_URL}/note/v1"
+JWT_BASE_URL = f"{BASE_URL}/token"
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,7 @@
 from .base import *
 
 DEBUG = False
+
+BASE_URL = "https://hdhwang.duckdns.org:8080/note/v1"
+API_BASE_URL = f"{BASE_URL}/note/v1"
+JWT_BASE_URL = f"{BASE_URL}/token"
